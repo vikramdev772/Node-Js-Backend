52b73db7a1db173e397acc0707dfb12928de1549
vikram <vikramdev772@gmail.com>
2024-02-14 13:14:32 +0530

    query paramas

---
+++ calc/index.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const snn = require("./sum"); // Correct the import statement
+const port = 8080;
+
+app.get("/get", (req, res) => res.send(" mango 🥭 apple 🍎🍏"));
+
+app.get("/s", (req, res) => {
+    const value = req.query.value;
+    let ans = "  the sum of n natural number : " + snn(value); // Correct the function call
+    res.status(200).send("Number : " + value + ans);
+    console.log("\n\t Result : "+(req.query.v1))
+    console.log("\n\t Result : "+(req.query.v2));
+});
+
+
+
+app.listen(port, () => console.log(`\n\t server is running on port ${port}`));
+
+
+// Result : 8
+
+// Result : 5
---
+++ calc/sum.js
@@ -0,0 +1,12 @@
+
+function snn(a){
+    let ans = 0;
+    for(let i=1;i<=a;i++){
+        ans+=i;
+    }
+    return ans;
+}
+
+// console.log(snn(5))
+
+module.exports = snn;
